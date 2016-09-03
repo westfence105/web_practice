6937c6bc10cbad8b20bfd9cfea9c8db4b08f9c7d
Togo Nishigaki <westfence95@gmail.com>
2016-09-03 22:33:00 +0900

    Create example php what show date and time

---
+++ date/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<meta charset="UTF-8">
+<html>
+<head>
+<title>date</title>
+</head>
+<body>
+	<?php
+		date_default_timezone_set("Asia/Tokyo");
+		print( date('Y年n月j日 G:i') );
+	?>
+</body>
+</html>
