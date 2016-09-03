68eaecac83c63acd229a08f4baa8f7e771547120
Togo Nishigaki <westfence95@gmail.com>
2016-09-03 22:31:46 +0900

    Create form example program

---
+++ form/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<meta charset="UTF-8">
+<html>
+<head>
+<title>input</title>
+</head>
+<body>
+	<form action="output.php" method="POST">
+		<table border="1" margin="5">
+			<tr>
+				<td>名前	</td>
+				<td><input type="text" name="name"></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
---
+++ form/output.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<meta charset="UTF-8">
+<html>
+<head>
+<title>output</title>
+</head>
+<body>
+	<table border="1" margin="5">
+		<tr>
+			<td>名前</td>
+			<td><?php print($_POST['name']); ?></td>
+		</tr>
+	</table>
+</body>
+</html>
